892160f2001d9094361df9fd8c54aea4a4286bb6
Mike Grabski <mike@workflow.com>
2013-11-20 01:50:05 -0500

    create the basics

---
+++ src/angular-idle.js
@@ -0,0 +1,17 @@
+/**
+ * Respond to idle users in AngularJS
+ * @version v0.1.0
+ * @link http://hackedbychinese.github.io/ng-idle
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+(function (window, angular, undefined) {
+    'use strict';
+
+    // register modules
+    var ngIdleSvc = angular.module('ngIdle.services', []);
+    angular.module('ngIdle', ['ngIdle.services']);
+
+    function $IdleProvider() {
+    	
+    }
+})(window, window.angular);
---
+++ src/angular-idle.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('ngIdle.', function() {
+    // helpers
+
+    describe('services', function() {
+        beforeEach(module('ngIdle.services'));
+    });
+});
