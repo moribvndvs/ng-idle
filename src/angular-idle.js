85fafde4426c6ddcbc526fccf7d8f28e80e200d8
Mike Grabski <mike@workflow.com>
2013-12-19 05:08:10 -0500

    Housekeeping

--- src/angular-idle.js
+++ src/angular-idle.js
@@ -1,6 +1,6 @@
 /**
  * Respond to idle users in AngularJS
- * @version v0.2.1
+ * @version v0.2.2
  * @link http://hackedbychinese.github.io/ng-idle
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
