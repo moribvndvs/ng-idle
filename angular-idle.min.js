40c6506ee32b474af8b5a514d1e909987df1a17b
Mike Grabski <mike@workflow.com>
2014-01-31 00:37:27 -0500

    version bump

--- angular-idle.min.js
+++ angular-idle.min.js
@@ -1,6 +1,6 @@
 /*** Directives and services for responding to idle users in AngularJS
 * @author Mike Grabski <me@mikegrabski.com>
-* @version v0.3.0
+* @version v0.3.1
 * @link https://github.com/HackedByChinese/ng-idle.git
 * @license MIT
 */
