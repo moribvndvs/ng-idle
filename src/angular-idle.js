67b4cbc7fbce297dfb3bd8a18fe70be5f90972c6
Mike Grabski <me@mikegrabski.com>
2014-11-27 03:48:48 -0500

    Fixes #36 Add touchstart to default interrupt events

--- src/angular-idle.js
+++ src/angular-idle.js
@@ -79,7 +79,7 @@
       idleDuration: 20 * 60, // in seconds (default is 20min)
       warningDuration: 30, // in seconds (default is 30sec)
       autoResume: true, // lets events automatically resume (unsets idle state/resets warning)
-      events: 'mousemove keydown DOMMouseScroll mousewheel mousedown touchstart',
+      events: 'mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove',
       keepalive: true
     };
 
