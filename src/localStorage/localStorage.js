7e500bb55e38367ab28443a6395de1793aa73974
RevanProdigalKnight <revan@devven.nl>
2015-03-31 14:22:47 -0400

    Update localStorage.js
    Changed ngIdle.localStorage factory into a service because there should only ever be one instance of it (There can only be one instance of window.localStorage)
    
    Replaced instances of "tryParseJson(value)" with "angular.fromJson(value)" and "JSON.stringify(value)" with "angular.toJson(value)" because in my experience angular.(to|from)Json works better.

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -1,34 +1,19 @@
 angular.module('ngIdle.localStorage', [])
-  .factory('IdleLocalStorage', ['$window', function($window) {
+  .service('IdleLocalStorage', ['$window', function($window) {
     var storage = $window.localStorage;
-
-    function tryParseJson(value) {
-      try {
-        return JSON.parse(value, function(key, value) {
-          var match = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
-          if (match) return new Date(value);
-
-          return value;
-        });
-      }
-      catch(e) {
-        return value;
-      }
-    }
-
+    
     return {
       set: function(key, value) {
-        storage.setItem('ngIdle.'+key, JSON.stringify(value));
+        storage.setItem('ngIdle.'+key, angular.toJson(value));
       },
       get: function(key) {
-        var raw = storage.getItem('ngIdle.'+key);
-        return tryParseJson(raw);
+        return angular.fromJson(storage.getItem('ngIdle.'+key));
       },
       remove: function(key) {
         storage.removeItem('ngIdle.'+key);
       },
       parseJson: function(raw) {
-        return tryParseJson(raw);
+        return angular.fromJson(raw);
       }
     };
   }]);
