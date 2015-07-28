da48f7338e69026656a4ef3b23a8f3ddbb431fd7
Ernesto Venencio <evenencio@edrans.com>
2015-07-28 12:43:56 -0300

    add broadcast for user activity

--- angular-idle.js
+++ angular-idle.js
@@ -180,7 +180,9 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           $rootScope.$broadcast('IdleWarn', state.countdown);
           state.countdown--;
         }
-
+        function userAction() {                      
+          $rootScope.$broadcast('IdleUserAction');   
+        }  
         function timeout() {
           stopKeepalive();
           $interval.cancel(state.idle);
@@ -279,6 +281,8 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
             if (options.timeout && this.isExpired()) {
               timeout();
               return;
+            } else {
+              userAction();
             }
 
             // note: you can no longer auto resume once we exceed the expiry; you will reset state by calling watch() manually
