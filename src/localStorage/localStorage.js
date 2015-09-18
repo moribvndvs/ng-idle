0f8630d2b3c2821ea177d518f3e57b4040efa68b
KARTHI THANGARAJ <karthi_thangaraj@apple.com>
2015-09-17 20:02:24 -0700

    Added fallback for local storage.

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -1,7 +1,44 @@
 angular.module('ngIdle.localStorage', [])
   .service('IdleLocalStorage', ['$window', function($window) {
-    var storage = $window.localStorage;
-    
+    var storage = null;
+
+    function AlternativeStorage() {
+      var strorageMap = {};
+
+      this.setItem = function (key, value) {
+          strorageMap[key] = value;
+      };
+
+      this.getItem = function (key) {
+          if(typeof strorageMap[key] !== 'undefined' ) {
+              return strorageMap[key];
+          }
+          return null;
+      };
+
+      this.removeItem = function (key) {
+          strorageMap[key] = undefined;
+      };
+    }
+
+    function getStorage() {
+      var storage = $window.localStorage;
+
+       try { 
+          localStorage.setItem("ngIdleStorage", ""); 
+          localStorage.removeItem("ngIdleStorage");
+          storage = localStorage;
+       } catch(err) { 
+          storage = new AlternativeStorage();
+       }
+        return storage;
+    }
+
+    // Safari, in Private Browsing Mode, looks like it supports localStorage but all calls to setItem
+    // throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
+    // to avoid the entire page breaking, without having to do a check at each usage of Storage.
+    storage = getStorage();
+
     return {
       set: function(key, value) {
         storage.setItem('ngIdle.'+key, angular.toJson(value));
@@ -13,4 +50,4 @@ angular.module('ngIdle.localStorage', [])
         storage.removeItem('ngIdle.'+key);
       }
     };
-  }]);
+}]);
