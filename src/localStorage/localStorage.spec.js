dbaf2355eb48d0dfa0ec30a19ea876c58117143f
Mike Grabski <me@mikegrabski.com>
2015-11-04 20:59:32 -0500

    fix failing specs when running in angular ^ 1.4

--- src/localStorage/localStorage.spec.js
+++ src/localStorage/localStorage.spec.js
@@ -12,33 +12,33 @@ describe('ngIdle', function() {
 
   beforeEach(module('ngIdle.localStorage'));
 
-  describe('LocalStorage service', function() {
+  describe('IdleLocalStorage service', function() {
     beforeEach(function() {
-      angular.module('app', function() {});
+      angular.module('app', []);
     });
 
-    var $window, LocalStorage;
-    beforeEach(inject(['$window', 'IdleLocalStorage', function(_$window_, _LocalStorage_) {
+    var $window, IdleLocalStorage;
+    beforeEach(inject(function(_$window_, _IdleLocalStorage_) {
       $window = _$window_;
-      LocalStorage = _LocalStorage_;
+      IdleLocalStorage = _IdleLocalStorage_;
 
       spyOn($window.localStorage, 'setItem').andCallThrough();
-    }]));
+    }));
 
     it ('set() should set value', function() {
-      LocalStorage.set('key', 1);
+      IdleLocalStorage.set('key', 1);
       expect($window.localStorage.setItem).toHaveBeenCalledWith('ngIdle.key', '1');
     });
 
     it ('get() should retrieve value as JSON', function() {
       spyOn($window.localStorage, 'getItem').andReturn('{"value": 1}');
-      var actual = LocalStorage.get('key');
+      var actual = IdleLocalStorage.get('key');
       expect(actual).toEqualData({value:1});
     });
 
     it ('remove() should remove key/value', function() {
       spyOn($window.localStorage, 'removeItem');
-      LocalStorage.remove('key');
+      IdleLocalStorage.remove('key');
       expect($window.localStorage.removeItem).toHaveBeenCalledWith('ngIdle.key');
     });
   });
