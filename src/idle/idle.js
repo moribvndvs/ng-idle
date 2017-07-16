d2e0b3069826c39cc024355e5463b7f6e02b74ed
David Federman <david.federman@outlook.com>
2017-07-16 13:53:31 -0700

    Undo reformatting

--- src/idle/idle.js
+++ src/idle/idle.js
@@ -1,6 +1,5 @@
 angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
-  .provider('Idle', function ()
-  {
+  .provider('Idle', function() {
     var options = {
       idle: 20 * 60, // in seconds (default is 20min)
       timeout: 30, // in seconds (default is 30sec)
@@ -14,45 +13,38 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
      *  Sets the number of seconds a user can be idle before they are considered timed out.
      *  @param {Number|Boolean} seconds A positive number representing seconds OR 0 or false to disable this feature.
      */
-    var setTimeout = this.timeout = function (seconds)
-    {
+    var setTimeout = this.timeout = function(seconds) {
       if (seconds === false) options.timeout = 0;
       else if (angular.isNumber(seconds) && seconds >= 0) options.timeout = seconds;
       else throw new Error('Timeout must be zero or false to disable the feature, or a positive integer (in seconds) to enable it.');
     };
 
-    this.interrupt = function (events)
-    {
+    this.interrupt = function(events) {
       options.interrupt = events;
     };
 
-    this.windowInterrupt = function (events)
-    {
+    this.windowInterrupt = function(events) {
       options.windowInterrupt = events;
     };
 
-    var setIdle = this.idle = function (seconds)
-    {
+    var setIdle = this.idle = function(seconds) {
       if (seconds <= 0) throw new Error('Idle must be a value in seconds, greater than 0.');
 
       options.idle = seconds;
     };
 
-    this.autoResume = function (value)
-    {
+    this.autoResume = function(value) {
       if (value === true) options.autoResume = 'idle';
       else if (value === false) options.autoResume = 'off';
       else options.autoResume = value;
     };
 
-    this.keepalive = function (enabled)
-    {
+    this.keepalive = function(enabled) {
       options.keepalive = enabled === true;
     };
 
     this.$get = ['$interval', '$log', '$rootScope', '$document', 'Keepalive', 'IdleLocalStorage', '$window',
-      function ($interval, $log, $rootScope, $document, Keepalive, LocalStorage, $window)
-      {
+      function($interval, $log, $rootScope, $document, Keepalive, LocalStorage, $window) {
         var state = {
           idle: null,
           timeout: null,
@@ -63,8 +55,7 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
 
         var id = new Date().getTime();
 
-        function startKeepalive()
-        {
+        function startKeepalive() {
           if (!options.keepalive) return;
 
           if (state.running) Keepalive.ping();
@@ -72,30 +63,25 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           Keepalive.start();
         }
 
-        function stopKeepalive()
-        {
+        function stopKeepalive() {
           if (!options.keepalive) return;
 
           Keepalive.stop();
         }
 
-        function toggleState()
-        {
+        function toggleState() {
           state.idling = !state.idling;
           var name = state.idling ? 'IdleStart' : 'IdleEnd';
 
-          if (state.idling)
-          {
+          if (state.idling) {
             $rootScope.$broadcast(name);
             stopKeepalive();
-            if (options.timeout)
-            {
+            if (options.timeout) {
               state.countdown = options.timeout;
               countdown();
               state.timeout = $interval(countdown, 1000, options.timeout, false);
             }
-          } else
-          {
+          } else {
             startKeepalive();
             $rootScope.$broadcast(name);
           }
@@ -103,19 +89,16 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           $interval.cancel(state.idle);
         }
 
-        function countdown()
-        {
+        function countdown() {
 
           // check not called when no longer idling
           // possible with multiple tabs
-          if (!state.idling)
-          {
+          if(!state.idling){
             return;
           }
 
           // countdown has expired, so signal timeout
-          if (state.countdown <= 0)
-          {
+          if (state.countdown <= 0) {
             timeout();
             return;
           }
@@ -125,13 +108,11 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           state.countdown--;
         }
 
-        function interrupted(anotherTab)
-        {
+        function interrupted(anotherTab) {
           $rootScope.$broadcast('IdleInterrupt', anotherTab);
         }
 
-        function timeout()
-        {
+        function timeout() {
           stopKeepalive();
           $interval.cancel(state.idle);
           $interval.cancel(state.timeout);
@@ -143,8 +124,7 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           $rootScope.$broadcast('IdleTimeout');
         }
 
-        function changeOption(self, fn, value)
-        {
+        function changeOption(self, fn, value) {
           var reset = self.running();
 
           self.unwatch();
@@ -152,59 +132,47 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           if (reset) self.watch();
         }
 
-        function getExpiry()
-        {
+        function getExpiry() {
           var obj = LocalStorage.get('expiry');
 
           return obj && obj.time ? new Date(obj.time) : null;
         }
 
-        function setExpiry(date)
-        {
+        function setExpiry(date) {
           if (!date) LocalStorage.remove('expiry');
-          else LocalStorage.set('expiry', { id: id, time: date });
+          else LocalStorage.set('expiry', {id: id, time: date});
         }
 
         var svc = {
-          _options: function ()
-          {
+          _options: function() {
             return options;
           },
-          _getNow: function ()
-          {
+          _getNow: function() {
             return new Date();
           },
-          getIdle: function ()
-          {
+          getIdle: function(){
             return options.idle;
           },
-          getTimeout: function ()
-          {
+          getTimeout: function(){
             return options.timeout;
           },
-          setIdle: function (seconds)
-          {
+          setIdle: function(seconds) {
             changeOption(this, setIdle, seconds);
           },
-          setTimeout: function (seconds)
-          {
+          setTimeout: function(seconds) {
             changeOption(this, setTimeout, seconds);
           },
-          isExpired: function ()
-          {
+          isExpired: function() {
             var expiry = getExpiry();
             return expiry !== null && expiry <= this._getNow();
           },
-          running: function ()
-          {
+          running: function() {
             return state.running;
           },
-          idling: function ()
-          {
+          idling: function() {
             return state.idling;
           },
-          watch: function (noExpiryUpdate)
-          {
+          watch: function(noExpiryUpdate) {
             $interval.cancel(state.idle);
             $interval.cancel(state.timeout);
 
@@ -220,8 +188,7 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
 
             state.idle = $interval(toggleState, options.idle * 1000, 0, false);
           },
-          unwatch: function ()
-          {
+          unwatch: function() {
             $interval.cancel(state.idle);
             $interval.cancel(state.timeout);
 
@@ -231,16 +198,13 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
 
             stopKeepalive();
           },
-          interrupt: function (anotherTab)
-          {
+          interrupt: function(anotherTab) {
             if (!state.running) return;
 
-            if (options.timeout && this.isExpired())
-            {
+            if (options.timeout && this.isExpired()) {
               timeout();
               return;
-            } else
-            {
+            } else {
               interrupted(anotherTab);
             }
 
@@ -252,15 +216,13 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
         var lastMove = {
           clientX: null,
           clientY: null,
-          swap: function (event)
-          {
-            var last = { clientX: this.clientX, clientY: this.clientY };
+          swap: function(event) {
+            var last = {clientX: this.clientX, clientY: this.clientY};
             this.clientX = event.clientX;
             this.clientY = event.clientY;
             return last;
           },
-          hasMoved: function (event)
-          {
+          hasMoved: function(event) {
             var last = this.swap(event);
             if (this.clientX === null || event.movementX || event.movementY) return true;
             else if (last.clientX != event.clientX || last.clientY != event.clientY) return true;
@@ -268,69 +230,53 @@ angular.module('ngIdle.idle', ['ngIdle.keepalive', 'ngIdle.localStorage'])
           }
         };
 
-        $document.find('html').on(options.interrupt, function (event)
-        {
-          if (event.type === 'mousemove' && event.originalEvent && event.originalEvent.movementX === 0 && event.originalEvent.movementY === 0)
-          {
+        $document.find('html').on(options.interrupt, function(event) {
+          if (event.type === 'mousemove' && event.originalEvent && event.originalEvent.movementX === 0 && event.originalEvent.movementY === 0) {
             return; // Fix for Chrome desktop notifications, triggering mousemove event.
           }
 
-          if (event.type !== 'mousemove' || lastMove.hasMoved(event))
-          {
+          if (event.type !== 'mousemove' || lastMove.hasMoved(event)) {
             svc.interrupt();
           }
         });
 
-        if (options.windowInterrupt)
-        {
+        if(options.windowInterrupt) {
           var eventList = options.windowInterrupt.split(' ');
-          var fn = function ()
-          {
+          var fn = function() {
             svc.interrupt();
           };
 
-          for (var i = 0; i < eventList.length; i++)
-          {
-            if ($window.addEventListener)
-            {
+          for(var i=0; i<eventList.length; i++) {
+            if ($window.addEventListener) {
               $window.addEventListener(eventList[i], fn, false);
-              $rootScope.$on('$destroy', function ()
-              {
+              $rootScope.$on('$destroy', function () {
                 $window.removeEventListener(eventList[i], fn, false);
               });
-            } else
-            {
+            } else {
               $window.attachEvent(eventList[i], fn);
-              $rootScope.$on('$destroy', function ()
-              {
+              $rootScope.$on('$destroy', function () {
                 $window.detachEvent(eventList[i], fn);
               });
             }
           }
         }
 
-        var wrap = function (event)
-        {
-          if (event.key === 'ngIdle.expiry' && event.newValue && event.newValue !== event.oldValue)
-          {
+        var wrap = function(event) {
+          if (event.key === 'ngIdle.expiry' && event.newValue && event.newValue !== event.oldValue) {
             var val = angular.fromJson(event.newValue);
             if (val.id === id) return;
             svc.interrupt(true);
           }
         };
 
-        if ($window.addEventListener)
-        {
+        if ($window.addEventListener) {
           $window.addEventListener('storage', wrap, false);
-          $rootScope.$on('$destroy', function ()
-          {
+          $rootScope.$on('$destroy', function () {
             $window.removeEventListener('storage', wrap, false);
           });
-        } else if ($window.attachEvent)
-        {
+        } else if ($window.attachEvent){
           $window.attachEvent('onstorage', wrap);
-          $rootScope.$on('$destroy', function ()
-          {
+          $rootScope.$on('$destroy', function () {
             $window.detachEvent('onstorage', wrap);
           });
         }
